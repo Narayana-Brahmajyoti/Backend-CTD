ca5f2c57a87b2f04f55fd58fce3c624961d5cda6
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-08-27 14:32:30 -0300

    Finalizando integrador VII- SpringBoot

--- integrador VII/clinica/src/main/java/com/example/clinica/ClinicaApplication.java
+++ integrador VII/clinica/src/main/java/com/example/clinica/ClinicaApplication.java
@@ -10,4 +10,8 @@ public class ClinicaApplication {
 		SpringApplication.run(ClinicaApplication.class, args);
 	}
 
+	// URL modelo para acessar no navegador
+	// http://localhost:8080/usuario?id=2&nome=Narayana&email=narayana@email.com&senha=123&nivelAcesso=Vip
+	// http://localhost:8080/dentista?id=1&nome=Narayana&email=narayana@email.com&numMatricula=010456&atendeConvenio=True
+
 }
--- integrador VII/clinica/src/main/java/com/example/clinica/controller/UsuarioController.java
+++ integrador VII/clinica/src/main/java/com/example/clinica/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import com.example.clinica.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 
 @RestController
