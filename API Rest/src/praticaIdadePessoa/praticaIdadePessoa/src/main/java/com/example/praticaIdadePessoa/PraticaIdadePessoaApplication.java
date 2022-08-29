0fe2cbe307962e0ef417ce1c1a61638d274b2060
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-08-29 12:26:06 -0300

    Prática desenvolvendo uma Api Rest

---
+++ API Rest/src/praticaIdadePessoa/praticaIdadePessoa/src/main/java/com/example/praticaIdadePessoa/PraticaIdadePessoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.praticaIdadePessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaIdadePessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaIdadePessoaApplication.class, args);
+	}
+
+}
---
+++ API Rest/src/praticaIdadePessoa/praticaIdadePessoa/src/test/java/com/example/praticaIdadePessoa/PraticaIdadePessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praticaIdadePessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaIdadePessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
